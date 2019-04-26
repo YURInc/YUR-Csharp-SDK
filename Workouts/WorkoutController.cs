e9b11c930884f974d897b4564505aaafeff4c49e
Ethan McCosby <ethan.mccosby@gmail.com>
2019-04-26 19:59:51 -0400

    Added Component for Easy Starting and Completion of workouts

---
+++ Workouts/WorkoutController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace YUR.SDK.Unity.Workouts
+{
+    [AddComponentMenu("YUR/Workout Controller",0)]
+    public class WorkoutController : MonoBehaviour
+    {
+        [Tooltip("The name of the workout or a way to identify it")]
+        public string WorkoutIdentifier;
+        public void StartWorkout()
+        {
+            Workout.StartingWorkout();
+        }
+
+        public void EndWorkout(string identifier)
+        {
+            Workout.EndingWorkout(identifier);
+        }
+
+        public void EndWorkout()
+        {
+            Workout.EndingWorkout(WorkoutIdentifier);
+        }
+
+    }
+}
